a6bcf9249596ca11d374483d78b7a2d095726299
Agoziem <chiagoziendukwe90@gmail.com>
2022-04-25 23:53:48 -0700

    changes

--- OKFSsite/settings.py
+++ OKFSsite/settings.py
@@ -134,12 +134,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL='/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS= [os.path.join(BASE_DIR, "assets"),]
+# STATIC_URL='/static/'
+# STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# STATICFILES_DIRS= [os.path.join(BASE_DIR, "assets"),]
 
-MEDIA_URL= '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+# MEDIA_URL= '/media/'
+# MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- OKFSsite/urls.py
+++ OKFSsite/urls.py
@@ -5,6 +5,7 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+
 urlpatterns = [
 	path('', home_view, name='home'),
     path('about/',about_view, name='about'),
